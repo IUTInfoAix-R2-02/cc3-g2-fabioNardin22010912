7b8b754a0e763542b8550215b25dd984eb78909c
fabioNardin22010912 <fabio.nardi@etu.univ-amu.fr>
2023-06-02 15:20:16 +0200

    partie 2 finie sauf points non colorés

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -17,6 +17,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -30,10 +31,13 @@ public class ToileController implements Initializable {
     private static int angleEnDegre = 60;
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
+    private static int originX = 200;
+    private static int originY = 200;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
     int getXRadarChart(double value, int axe ){
@@ -45,14 +49,29 @@ public class ToileController implements Initializable {
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
-    void tracePoint(double value, int axe){
+    void changePosPoint(int x, int y ,Circle c){
+        c.setCenterX(x);
+        c.setCenterY(y);
+    }
+    void calcPosPoint(double value, int axe, Circle c){
         int x;
         int y;
         x=getXRadarChart(value,axe);
         y=getYRadarChart(value,axe);
-        System.out.println(x);
-        System.out.println(y);
+        changePosPoint(x,y,c);
     }
+    @FXML
+    Circle c1;
+    @FXML
+    Circle c2;
+    @FXML
+    Circle c3;
+    @FXML
+    Circle c4;
+    @FXML
+    Circle c5;
+    @FXML
+    Circle c6;
     @FXML
     TextField val1;
     String v1;
@@ -73,15 +92,31 @@ public class ToileController implements Initializable {
     String v6;
 
     @FXML
-    private void Tracer(ActionEvent event){
+    private void tracer(ActionEvent event){
         v1=val1.getText();
         v2=val2.getText();
         v3=val3.getText();
         v4=val4.getText();
         v5=val5.getText();
         v6=val6.getText();
-        tracePoint(Integer.valueOf(v1),1);
+        if (v1 !=""){calcPosPoint(Integer.valueOf(v1),1,c1);}
+        if (v2 !=""){calcPosPoint(Integer.valueOf(v2),2,c2);}
+        if (v3 !="")calcPosPoint(Integer.valueOf(v3),3,c3);
+        if (v4 !="")calcPosPoint(Integer.valueOf(v4),4,c4);
+        if (v5 !="")calcPosPoint(Integer.valueOf(v5),5,c5);
+        if (v6 !="")calcPosPoint(Integer.valueOf(v6),6,c6);
 
     }
+    @FXML
+    private void vider(ActionEvent e){
+        // changer la value des coord de tt les points en l'origine cad 200 200
 
+        changePosPoint(originX,originY,c1);
+        changePosPoint(originX,originY,c2);
+        changePosPoint(originX,originY,c3);
+        changePosPoint(originX,originY,c4);
+        changePosPoint(originX,originY,c5);
+        changePosPoint(originX,originY,c6);
+
+    }
 }
